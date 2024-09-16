218284d456e4a039d6a7de92adef61d6544a4af9
carsoninho <james.87.carson@gmail.com>
2024-09-16 15:19:29 +0700

    Decide on functions for DOM manipulation
    Written out in notebook
    
    Need to test out set page and design / test contact

---
+++ src/functions.js
@@ -0,0 +1,27 @@
+function clearDisplay() {
+
+}
+
+function renderHomeAboutUs () {
+
+}
+
+function renderMenuMaster () {
+
+}
+
+function appendSet() {
+
+}
+
+function appendFood() {
+
+}
+
+function appendDrinks() {
+
+}
+
+function renderContact() {
+    
+}
