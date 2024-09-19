7174ed0b00094032bece00168e10aaadb0249613
carsoninho <james.87.carson@gmail.com>
2024-09-19 12:16:39 +0700

    Create images.js and import/export files

---
+++ src/images.js
@@ -0,0 +1,57 @@
+// Import images for Asam Pedas Set
+import asamPedasAyamImage from './images/menu_items/set/asam_pedas_ayam.png';
+import asamPedasIkanImage from './images/menu_items/set/asam_pedas_ikan.png';
+
+// Import images for Food items
+import nasiGorengBiasaImage from './images/menu_items/food/nasi_goreng_biasa.jpeg';
+import nasiGorengCiliPadiImage from './images/menu_items/food/nasi_goreng_cili_padi.jpeg';
+import nasiGorengKampungImage from './images/menu_items/food/nasi_goreng_kampung.jpeg';
+import nasiGorengPattayaImage from './images/menu_items/food/nasi_goreng_pattaya.jpeg';
+import nasiAyamGorengImage from './images/menu_items/food/nasi_ayam_goreng.png';
+import meeGorengImage from './images/menu_items/food/mee_goreng.jpeg';
+import meeBandungImage from './images/menu_items/food/mee_bandung.jpg';
+import meeKungfuImage from './images/menu_items/food/mee_kungfu.jpg';
+import tomYamAyamImage from './images/menu_items/food/tom_yam_ayam.jpeg';
+import tomYamDagingImage from './images/menu_items/food/tom_yam_daging.jpeg';
+
+// Import images for Drinks
+import miloImage from './images/menu_items/drinks/milo.jpeg';
+import tehImage from './images/menu_items/drinks/teh.jpeg';
+import tehOImage from './images/menu_items/drinks/teh_o.jpg';
+import tehOLimauImage from './images/menu_items/drinks/teh_o_limau.jpeg';
+import kopiImage from './images/menu_items/drinks/kopi.jpg';
+import kopiOImage from './images/menu_items/drinks/kopi_o.jpg';
+import limauImage from './images/menu_items/drinks/limau.jpg';
+import jusAppleImage from './images/menu_items/drinks/jus_apple.jpeg';
+import jusOrangeImage from './images/menu_items/drinks/jus_orange.jpg';
+import jusCarrotImage from './images/menu_items/drinks/jus_carrot.jpeg';
+
+export const images = {
+  // Asam Pedas Set
+  asamPedasAyam: asamPedasAyamImage,
+  asamPedasIkan: asamPedasIkanImage,
+
+  // Food Set
+  nasiGorengBiasa: nasiGorengBiasaImage,
+  nasiGorengCiliPadi: nasiGorengCiliPadiImage,
+  nasiGorengKampung: nasiGorengKampungImage,
+  nasiGorengPattaya: nasiGorengPattayaImage,
+  nasiAyamGoreng: nasiAyamGorengImage,
+  meeGoreng: meeGorengImage,
+  meeBandung: meeBandungImage,
+  meeKungfu: meeKungfuImage,
+  tomYamAyam: tomYamAyamImage,
+  tomYamDaging: tomYamDagingImage,
+
+  // Drinks Set
+  milo: miloImage,
+  teh: tehImage,
+  tehO: tehOImage,
+  tehOLimau: tehOLimauImage,
+  kopi: kopiImage,
+  kopiO: kopiOImage,
+  limau: limauImage,
+  jusApple: jusAppleImage,
+  jusOrange: jusOrangeImage,
+  jusCarrot: jusCarrotImage
+};
